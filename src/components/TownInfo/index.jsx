630ba75b7fed8ce10e997136dfa79113a143f0db
JZU0 <ljzh1030@gmail.com>
2024-09-01 18:15:43 +0900

    feat: add town info component

---
+++ src/components/TownInfo/index.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useQuery } from 'react-query';
+import { fetchTownInfo } from '../../apis/memberApi';
+import {
+  TownInfoContainer,
+  InfoBox,
+  StatsBox,
+  TownName,
+  TownDescription,
+  InfoWrapper,
+  StatItem,
+  StatIcon,
+  StatText,
+  RankBadge,
+} from './styled';
+import peopleIcon from '../../assets/images/people.svg';
+import moaIcon from '../../assets/images/main_moa.svg';
+
+const TownInfo = () => {
+  const { data: townInfo = {} } = useQuery('townInfo', fetchTownInfo);
+
+  const getRank = (totalTax) => {
+    if (totalTax > 1000) return '4';
+    if (totalTax > 500) return '3';
+    if (totalTax > 100) return '2';
+    return '1';
+  };
+
+  return (
+    <TownInfoContainer>
+      <InfoBox>
+        <RankBadge>{getRank(townInfo.totalTax)}</RankBadge>
+        <InfoWrapper>
+          <TownName>{townInfo.name}</TownName>
+          <TownDescription>{townInfo.description}</TownDescription>
+        </InfoWrapper>
+      </InfoBox>
+      <StatsBox>
+        <StatItem>
+          <StatIcon src={peopleIcon} alt="Total Members" />
+          <StatText>{townInfo.totalMembers}</StatText>
+        </StatItem>
+        <StatItem>
+          <StatIcon src={moaIcon} alt="Total Tax" />
+          <StatText>{townInfo.totalTax}</StatText>
+        </StatItem>
+      </StatsBox>
+    </TownInfoContainer>
+  );
+};
+
+export default TownInfo;
