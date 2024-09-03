7415c8dc87b1c98d6522effdcf8c17dc2ab8869f
JZU0 <ljzh1030@gmail.com>
2024-09-03 13:53:30 +0900

    feat: add protect route

--- src/pages/Mypage/index.jsx
+++ src/pages/Mypage/index.jsx
@@ -1,19 +1,22 @@
 import React from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Header, JobInfo, Passbook } from '../../components';
 
 import { Container, ClosetTitle, ClosetImage } from './styled';
 import closet from '../../assets/images/closet.svg';
 
 const Mypage = () => {
-  const navigate = useNavigate();
   return (
     <Container>
       <Header />
       <JobInfo />
-      <Passbook />
+      <Link to='/account'>
+        <Passbook />
+      </Link>
       <ClosetTitle>나의 옷장</ClosetTitle>
-      <ClosetImage src={closet} />
+      <Link to='/closet-entry'>
+        <ClosetImage src={closet} />
+      </Link>
     </Container>
   );
 };
