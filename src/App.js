ad304702107726c5a8899b0491d4fea7828b1de2
JAESEONGIM <bigstar2878@gmail.com>
2024-09-06 11:42:45 +0900

    feat: dept 페이지에 구글 애널리틱스 추가

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import { RecoilRoot } from 'recoil';
+import React, { useEffect } from 'react';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import Router from './Router';
+import ReactGA from 'react-ga4';
 
 import './styles/reset.css';
 
 const queryClient = new QueryClient();
+const GA_TRACKING_ID = process.env.REACT_APP_GA_TRACKING_ID;
 
+// function App() {
+//   return (
+//     <RecoilRoot>
+//       <QueryClientProvider client={queryClient}>
+//         <Router />
+//       </QueryClientProvider>
+//     </RecoilRoot>
+//   );
+// }
 function App() {
+  useEffect(() => {
+    ReactGA.initialize(GA_TRACKING_ID);
+  }, []);
+
   return (
     <RecoilRoot>
       <QueryClientProvider client={queryClient}>
